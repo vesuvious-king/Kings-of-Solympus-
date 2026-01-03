7982c6269409058f898b7753af4be7cdbdb8409e
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-02 20:08:51 -0800

    Delete app/app directory

--- app/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Kings of Solympus</h1>
-      <p>Deployment test successful.</p>
-    </main>
-  );
-}
--- app/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1>Kings of Solympus</h1>;
-}
