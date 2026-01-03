7323fd3b8fb42469de95a8995e5f6fb379cae6a5
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-03 09:03:53 -0800

    Create Home component with main content

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "60px", fontFamily: "serif" }}>
+      <h1>Kings of Solympus</h1>
+      <p>The throne awaits.</p>
+    </main>
+  );
+}
