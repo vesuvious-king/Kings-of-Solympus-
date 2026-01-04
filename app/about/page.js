eded8d546a95a51d7e26c1d1b097c47508dbae35
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 11:00:06 -0800

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,21 @@
+export default function About() {
+  return (
+    <main style={{ padding: "2rem", color: "gold" }}>
+      <h1>About Us</h1>
+
+      <p>
+        Kings of Solympus is a dark medieval universe built through art,
+        story, and myth.
+      </p>
+
+      <p>
+        This project exists to merge narrative, visual art,
+        and community into one evolving world.
+      </p>
+
+      <p>
+        Contact information will be added here.
+      </p>
+    </main>
+  );
+}
