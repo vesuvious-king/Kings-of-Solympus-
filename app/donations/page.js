a798f945b044767b89be2b4d8d2affbaf572eaa8
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 10:59:26 -0800

    Create page.js

---
+++ app/donations/page.js
@@ -0,0 +1,16 @@
+export default function Donations() {
+  return (
+    <main style={{ padding: "2rem", color: "gold" }}>
+      <h1>Donations</h1>
+
+      <p>
+        Donations help support the creation of the art, the book,
+        and the continued expansion of this world.
+      </p>
+
+      <p>
+        More details will be added here.
+      </p>
+    </main>
+  );
+}
