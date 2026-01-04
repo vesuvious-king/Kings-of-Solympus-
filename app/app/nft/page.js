12418cae2269547de7e7296650c8526b483a4849
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 10:40:49 -0800

    Create page.js

---
+++ app/app/nft/page.js
@@ -0,0 +1,14 @@
+export default function NFTPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>THE NFT</h1>
+
+      <p>
+        Kings of Solympus NFTs are characters forged in war,
+        betrayal, and dark energy.
+        <br />
+        Each piece exists within the same brutal universe.
+      </p>
+    </main>
+  );
+}
