bae3cc569bbaf0231834b3fb921e24813ba223cb
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 11:20:21 -0800

    Create page.js

---
+++ app/faq/page.js
@@ -0,0 +1,8 @@
+export default function FAQ() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>FAQ</h1>
+      <p>Frequently asked questions about Kings of Solympus.</p>
+    </main>
+  );
+}
