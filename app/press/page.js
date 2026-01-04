8c4efe21779323b8d8f3d4a71881f02cb3a65ffc
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 11:18:54 -0800

    Create page.js

---
+++ app/press/page.js
@@ -0,0 +1,8 @@
+export default function Press() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Press</h1>
+      <p>Press resources, announcements, and media will live here.</p>
+    </main>
+  );
+}
