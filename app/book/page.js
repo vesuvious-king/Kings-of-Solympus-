60d300cf3688e1fd2b1b0eb18bcfc1c3abb6c74b
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 10:38:31 -0800

    Update page.js

--- app/book/page.js
+++ app/book/page.js
@@ -1,1 +1,14 @@
+export default function BookPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "inherit" }}>
+      <h1>THE BOOK</h1>
 
+      <p>
+        The written chronicle of Kings of Solympus.
+        <br />
+        This book expands the lore, the violence, the creatures,
+        and the broken kingdoms that shaped the world.
+      </p>
+    </main>
+  );
+}
