0ad79ff7991b2c7513e7f3ecde2eb7b9563961c4
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 10:46:45 -0800

    Update page.js

--- app/book/page.js
+++ app/book/page.js
@@ -1,13 +1,15 @@
 export default function BookPage() {
   return (
-    <main style={{ padding: "40px", fontFamily: "inherit" }}>
+    <main style={{ padding: "60px", color: "#e6c36a" }}>
       <h1>THE BOOK</h1>
 
       <p>
-        The written chronicle of Kings of Solympus.
-        <br />
-        This book expands the lore, the violence, the creatures,
-        and the broken kingdoms that shaped the world.
+        This page is dedicated to the written chronicle of Kings of Solympus.
+        The story, the lore, the violence, and the mythology will live here.
+      </p>
+
+      <p>
+        More will be revealed.
       </p>
     </main>
   );
