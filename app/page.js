0b6f5648e4a253420e761d99b7b611fc69c27f7f
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 10:15:03 -0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,27 +1,13 @@
 export default function Home() {
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        backgroundColor: "black",
-        color: "#f5d77a",
-        fontFamily: "'Comic Neue', cursive",
-        textAlign: "center",
-        paddingTop: "30vh",
-        textShadow: "3px 3px 0px #000"
-      }}
-    >
-      <h1 style={{ fontSize: "72px", marginBottom: "24px" }}>
-        KINGS OF SOLYMPUS
-      </h1>
+    <main style={{ paddingTop: "40px", whiteSpace: "pre-wrap" }}>
+      From a forgotten age comes a land full of mystery and chaos. A story of an ancient land and characters and creatures forged in the wastelands. Bloody and mythical. A medieval horror tale whispered about in torch-lit halls and carved into massive walls of stone. This is a story of violence and survival. Broken kingdoms and buried legends. All beings in this story carry the weight of battle, betrayal, and dark energy. One single mutated human shapes the fate of this world.
 
-      <p style={{ fontSize: "24px", maxWidth: "720px", margin: "0 auto" }}>
-        A mythic archive of goblins and skellies.
-        <br />
-        Born unfinished. Reforged by hand.
-        <br />
-        Gold is fashion. Crowns are boredom.
-      </p>
+
+              THE BOOK
+
+
+              THE NFT
     </main>
   );
 }
