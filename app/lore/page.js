ea96aef8076073c76815146a5e567bf2b02c078b
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 11:20:53 -0800

    Create page.js

---
+++ app/lore/page.js
@@ -0,0 +1,8 @@
+export default function Lore() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Lore</h1>
+      <p>The myths, history, and dark legends of this world.</p>
+    </main>
+  );
+}
