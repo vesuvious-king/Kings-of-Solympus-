b0da9d65eb47fc7523a24e0fa435b4803199861b
vesuvious-king <sendjonstuff@hotmail.com>
2026-01-04 10:42:59 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,43 +1,79 @@
 export const metadata = {
-  title: "Kings of Olympus",
-  description: "A medieval horror mythos",
+  title: "Kings of Solympus",
+  description: "A mythic archive of broken kingdoms and buried legends",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, padding: 0, backgroundColor: "black", color: "#d4b46a" }}>
-
-        {/* LEFT NAVIGATION */}
+      <body
+        style={{
+          margin: 0,
+          backgroundColor: "black",
+          color: "#e6c98a",
+          fontFamily: "inherit",
+        }}
+      >
+        {/* LEFT MENU */}
         <nav
           style={{
             position: "fixed",
             top: "20px",
             left: "20px",
-            fontFamily: "inherit",
-            zIndex: 1000,
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            fontSize: "14px",
           }}
         >
-          <ul style={{ listStyle: "none", padding: 0, margin: 0, lineHeight: "1.8em" }}>
-            <li><a href="/" style={{ color: "#d4b46a", textDecoration: "none" }}>Home</a></li>
-            <li><a href="/book" style={{ color: "#d4b46a", textDecoration: "none" }}>The Book</a></li>
-            <li><a href="/nft" style={{ color: "#d4b46a", textDecoration: "none" }}>The NFT</a></li>
-            <li><a href="/art-archive" style={{ color: "#d4b46a", textDecoration: "none" }}>Art Archive</a></li>
-            <li><a href="/community" style={{ color: "#d4b46a", textDecoration: "none" }}>Join Our Community</a></li>
-            <li><a href="/promotion" style={{ color: "#d4b46a", textDecoration: "none" }}>Art Promotion Area</a></li>
-            <li><a href="/merch" style={{ color: "#d4b46a", textDecoration: "none" }}>Merchandise</a></li>
-            <li><a href="/magic-eden" style={{ color: "#d4b46a", textDecoration: "none" }}>Magic Eden</a></li>
-            <li><a href="/roadmap" style={{ color: "#d4b46a", textDecoration: "none" }}>Roadmap</a></li>
-            <li><a href="/donations" style={{ color: "#d4b46a", textDecoration: "none" }}>Donations</a></li>
-            <li><a href="/about" style={{ color: "#d4b46a", textDecoration: "none" }}>About / Contact</a></li>
-          </ul>
+          <a href="/" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            Home
+          </a>
+          <a href="/book" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            The Book
+          </a>
+          <a href="/nft" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            The NFT
+          </a>
+          <a href="/archive" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            Art Archive
+          </a>
+          <a href="/preview" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            Join Our Community
+          </a>
+          <a href="/preview" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            Art Promotion Area
+          </a>
+          <a href="/preview" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            Merchandise
+          </a>
+          <a
+            href="https://magiceden.io"
+            target="_blank"
+            style={{ color: "#e6c98a", textDecoration: "none" }}
+          >
+            Magic Eden
+          </a>
+          <a href="/preview" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            Roadmap
+          </a>
+          <a href="/preview" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            Donations
+          </a>
+          <a href="/preview" style={{ color: "#e6c98a", textDecoration: "none" }}>
+            About / Contact
+          </a>
         </nav>
 
         {/* PAGE CONTENT */}
-        <main style={{ paddingTop: "80px" }}>
+        <main
+          style={{
+            paddingLeft: "160px",
+            paddingTop: "40px",
+          }}
+        >
           {children}
         </main>
-
       </body>
     </html>
   );
